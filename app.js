49fb65d39ff60da813b72fe4dbb7a4373912bc10
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-07 15:44:24 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const cors = require("cors");
 // Router
 const pizzasRouter = require("./routers/pizzasRouter");
 app.use(express.static("public"));
 app.use(express.json());
+
+// Cors
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server della mia pizzeria");
